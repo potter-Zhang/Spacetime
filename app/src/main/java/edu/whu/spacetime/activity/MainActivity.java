55830f506408063d4f6db2df23589f9b617673db
whu_xyw <2961706822@qq.com>
2024-03-17 20:46:59 +0800

    合并页面

--- app/src/main/java/edu/whu/spacetime/activity/MainActivity.java
+++ app/src/main/java/edu/whu/spacetime/activity/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.whu.spacetime.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
 
 import edu.whu.spacetime.R;
+import edu.whu.spacetime.adapter.MyFragmentPagerAdapter;
+import edu.whu.spacetime.fragment.NoteBrowserFragment;
+import edu.whu.spacetime.fragment.TodoBrowserFragment;
+import edu.whu.spacetime.fragment.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,68 +28,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // initNavigation();
-        // initViewPager();
+        initNavigation();
+        initViewPager();
     }
 
-//    private void initViewPager() {
-//        viewpager = findViewById(R.id.id_viewpager);
-//        ArrayList<Fragment> fragments = new ArrayList<>();
-//        // 在这里添加fragment
-//        fragments.add(UserFragment.newInstance());
-//        fragments.add(UserFragment.newInstance());
-//        fragments.add(UserFragment.newInstance());
-//        MyFragmentPagerAdapter myFragmentPagerAdapter = new MyFragmentPagerAdapter(
-//                getSupportFragmentManager(), getLifecycle(), fragments);
-//        viewpager.setAdapter(myFragmentPagerAdapter);
-//
-//        viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                super.onPageSelected(position);
-//                switch (position)
-//                {
-//                    case 0:
-//                        nav.setSelectedItemId(R.id.navigation_item1);
-//                        break;
-//                    case 1:
-//                        nav.setSelectedItemId(R.id.navigation_item2);
-//                        break;
-//                    case 2:
-//                        nav.setSelectedItemId(R.id.navigation_item3);
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-//
-//        });
-//    }
-//
-//    // 初始化navigationview，把每个item绑定到一个fragment上
-//    private void initNavigation() {
-//        nav = findViewById(R.id.nav_view);
-//        nav.setItemIconTintList(null);
-//        // navigation绑定到viewpager上
-//        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                int id = item.getItemId();
-//                if(id == R.id.navigation_item1){
-//                    viewpager.setCurrentItem(0);
-//                    return true;
-//                }
-//                else if(id == R.id.navigation_item2){
-//                    viewpager.setCurrentItem(1);
-//                    return true;
-//                }
-//                else if(id == R.id.navigation_item3){
-//                    viewpager.setCurrentItem(2);
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//    }
+    private void initViewPager() {
+        viewpager = findViewById(R.id.id_viewpager);
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        // 在这里添加fragment
+        fragments.add(NoteBrowserFragment.newInstance(1));
+        fragments.add(new TodoBrowserFragment());
+        fragments.add(UserFragment.newInstance());
+        MyFragmentPagerAdapter myFragmentPagerAdapter = new MyFragmentPagerAdapter(
+                getSupportFragmentManager(), getLifecycle(), fragments);
+        viewpager.setAdapter(myFragmentPagerAdapter);
+
+        viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position)
+                {
+                    case 0:
+                        nav.setSelectedItemId(R.id.navigation_item1);
+                        break;
+                    case 1:
+                        nav.setSelectedItemId(R.id.navigation_item2);
+                        break;
+                    case 2:
+                        nav.setSelectedItemId(R.id.navigation_item3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        });
+    }
+
+    // 初始化navigationview，把每个item绑定到一个fragment上
+    private void initNavigation() {
+        nav = findViewById(R.id.nav_view);
+        nav.setItemIconTintList(null);
+        // navigation绑定到viewpager上
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.navigation_item1){
+                    viewpager.setCurrentItem(0);
+                    return true;
+                }
+                else if(id == R.id.navigation_item2){
+                    viewpager.setCurrentItem(1);
+                    return true;
+                }
+                else if(id == R.id.navigation_item3){
+                    viewpager.setCurrentItem(2);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 }
