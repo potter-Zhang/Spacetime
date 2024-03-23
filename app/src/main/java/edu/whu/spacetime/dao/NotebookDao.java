0a234be17c43ce4dbfec0ffd065cbfe3c80d2e39
@little__prince <13510222833@139.com>
2024-03-23 19:08:53 +0800

    resize editor

--- app/src/main/java/edu/whu/spacetime/dao/NotebookDao.java
+++ app/src/main/java/edu/whu/spacetime/dao/NotebookDao.java
@@ -1,8 +1,25 @@
 package edu.whu.spacetime.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import edu.whu.spacetime.domain.Note;
+import edu.whu.spacetime.domain.Notebook;
 
 @Dao
 public interface NotebookDao {
+    @Insert
+    void insertNotebook(Notebook... notebook);
+    @Update
+    void updateNotebook(Notebook... notebook);
+    @Delete
+    void deleteNotebook(Notebook... notebook);
 
+    @Query("SELECT * FROM notebook WHERE userId = :userId")
+    List<Notebook> getNotebookByUserId(int userId);
 }
