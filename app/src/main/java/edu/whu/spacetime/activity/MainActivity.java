465f70d5c8e5e692a4207f56ab7e5aafdacae1bc
郭钦耀 <2434494650@qq.com>
2024-04-17 22:06:15 +0800

    界面优化调整

--- app/src/main/java/edu/whu/spacetime/activity/MainActivity.java
+++ app/src/main/java/edu/whu/spacetime/activity/MainActivity.java
@@ -100,7 +100,8 @@ public class MainActivity extends AppCompatActivity {
         states[1] = new int[] {-android.R.attr.state_checked};
 
         //状态对应颜色值（按下，默认）
-        int[] colors = new int[] {Color.parseColor("#8ACF19"),Color.GRAY};
+        //String color = "#".concat(getColor(R.color.main_theme));
+        int[] colors = new int[] {getColor(R.color.bottom_item_checked), Color.GRAY};
         ColorStateList colorList = new ColorStateList(states, colors);
         nav.setItemTextColor(colorList);
         viewpager.setUserInputEnabled(false);
